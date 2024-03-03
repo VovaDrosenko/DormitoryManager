0fdc46ea9e8151d17ef48a19e3584dd44542aee4
Volodymyr <vovadrosenko@gmail.com>
2024-03-03 20:56:09 +0200

    init

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+using DormitoryManager.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace DormitoryManager.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+        public IActionResult Creators()
+        {
+            return View();
+        }
+
+        public IActionResult Form()
+        {
+            return View();
+        }
+        public IActionResult Documents()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ wwwroot/js/site.js
@@ -0,0 +1,9 @@
+﻿document.addEventListener('DOMContentLoaded', function () {
+    var currentPage = '@ViewContext.RouteData.Values["Action"]';
+    var links = document.querySelectorAll('#sidebar .nav-link');
+    links.forEach(function (link) {
+        if (link.getAttribute('asp-action') === currentPage) {
+            link.classList.add('active');
+        }
+    });
+});
