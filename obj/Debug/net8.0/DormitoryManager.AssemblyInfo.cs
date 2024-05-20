900c7f0533b296ae89d86846fb5503e35e1125f7
alexvoron15 <sasha.vorona2015@gmail.com>
2024-05-21 00:06:00 +0300

    Revert "."
    This reverts commit 89cbaa89d8668ae2f37eb2cff754fbcfb386ef11.

--- obj/Debug/net8.0/DormitoryManager.AssemblyInfo.cs
+++ obj/Debug/net8.0/DormitoryManager.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DormitoryManager")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+98de6b1708fd478259cdbdceec9de01f7b53103e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a94cd9bdf9b7b352e342ea5ef13ef8f615ab4239")]
 [assembly: System.Reflection.AssemblyProductAttribute("DormitoryManager")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DormitoryManager")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
