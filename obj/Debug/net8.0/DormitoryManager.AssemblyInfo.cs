b5b074ee17014686362dfce83175b71df794fb0a
alexvoron15 <sasha.vorona2015@gmail.com>
2024-05-20 18:04:14 +0300

    Users

--- obj/Debug/net8.0/DormitoryManager.AssemblyInfo.cs
+++ obj/Debug/net8.0/DormitoryManager.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DormitoryManager")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a94cd9bdf9b7b352e342ea5ef13ef8f615ab4239")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2b425a38c5bdfe928dd5b5880635605d346da0ff")]
 [assembly: System.Reflection.AssemblyProductAttribute("DormitoryManager")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DormitoryManager")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
