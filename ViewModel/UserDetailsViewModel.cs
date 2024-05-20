a35e8f91a702f1e78df3b2d960ded694ed75039c
alexvoron15 <sasha.vorona2015@gmail.com>
2024-05-21 00:10:52 +0300

    Updated workers functional

---
+++ ViewModel/UserDetailsViewModel.cs
@@ -0,0 +1,11 @@
+﻿using DormitoryManager.Models.DTO_s.Dormitory;
+using DormitoryManager.Models.DTO_s.Faculty;
+using DormitoryManager.Models.DTO_s.User;
+
+namespace DormitoryManager.ViewModel {
+    public class UserDetailsViewModel {
+        public List<UsersDto> users { get; set; }
+        public List<DormitoryDto>? dormitory { get; set; }
+        public List<FacultiesDto>? faculties { get; set; }
+    }
+}
